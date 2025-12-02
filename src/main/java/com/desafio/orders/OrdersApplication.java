eb53af818e756d19403b39bc0d55cda800116eec
Matheus <matheus.leite.carneiro@gmail.com>
2025-12-02 20:48:21 -0300

    feat: read order input and print total, implementing CommandLineRunner

--- src/main/java/com/desafio/orders/OrdersApplication.java
+++ src/main/java/com/desafio/orders/OrdersApplication.java
@@ -1,13 +1,39 @@
 package com.desafio.orders;
 
+import com.desafio.orders.entities.Order;
+import com.desafio.orders.services.OrderService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class OrdersApplication {
+import java.util.Locale;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrdersApplication.class, args);
+@SpringBootApplication
+public class OrdersApplication implements CommandLineRunner {
+
+    private final OrderService orderService;
+
+    public OrdersApplication(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(OrdersApplication.class, args);
 	}
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        int code = sc.nextInt();
+        double basic = sc.nextDouble();
+        double discount = sc.nextDouble();
+        Order order = new Order(code, basic, discount);
+        System.out.println("Pedido código " + order.getCode());
+        System.out.printf("Valor total: R$%.2f%n", orderService.total(order));
+        sc.close();
+    }
 }
