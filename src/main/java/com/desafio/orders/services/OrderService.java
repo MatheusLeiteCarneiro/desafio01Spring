27a8dfbf95ed5e180991762d8d5db841adaeee38
Matheus <matheus.leite.carneiro@gmail.com>
2025-12-02 20:07:43 -0300

    chore: dependency injection of the shippingService

--- src/main/java/com/desafio/orders/services/OrderService.java
+++ src/main/java/com/desafio/orders/services/OrderService.java
@@ -1,4 +1,14 @@
 package com.desafio.orders.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
+
+    private ShippingService shippingService;
+
+    public OrderService(ShippingService shippingService) {
+        this.shippingService = shippingService;
+    }
+
 }
