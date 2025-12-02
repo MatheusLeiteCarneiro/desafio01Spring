3406caeb9cab25b3f15a7f11045efd6c9d3fe1dd
Matheus <matheus.leite.carneiro@gmail.com>
2025-12-02 20:03:23 -0300

    feat: create ShippingService service class and shipment method to calculate the shipment fee

--- src/main/java/com/desafio/orders/services/ShippingService.java
+++ src/main/java/com/desafio/orders/services/ShippingService.java
@@ -1,4 +1,23 @@
 package com.desafio.orders.services;
 
+import com.desafio.orders.entities.Order;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ShippingService {
+    private static final double shippingFeeUnder100 = 20.00;
+    private static final double shippingFeeUnder200 = 12.00;
+
+    public double shipment(Order order) {
+        double basicPrice = order.getBasic();
+
+        if (basicPrice < 100.00) {
+            return shippingFeeUnder100;
+        } else if (basicPrice >= 100.00 && basicPrice < 200.00) {
+            return shippingFeeUnder200;
+        } else {
+            return 0.0;
+        }
+
+    }
 }
